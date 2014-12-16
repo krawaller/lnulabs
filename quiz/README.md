e811acdf1e1af98b3cc9119d8166f41ef2c8fb70
KungD <david@krawaller.se>
2014-12-16 14:44:19 +0100

    added quiz lab

---
+++ quiz/README.md
@@ -0,0 +1,5 @@
+##Idiomatisk (eller snarare Davidig) implementering av The Quiz
+
+Se labben live [här](http://blog.krawaller.se/lnulabs/quiz). Labbinstruktionen finns [här](https://coursepress.lnu.se/kurs/webbteknik-i/laborationer/l04-validering/).
+
+Uppbyggd enligt samma filosofi som Labby Mezzage, inga konstigheter.
---
+++ quiz/quiz.js
@@ -0,0 +1,83 @@
+Quiz = function(selector){
+	function create(tag,content,listener,event){
+		var node = document.createElement(tag);
+		node.innerHTML = content || "";
+		if (listener){ node.addEventListener(event||"click",listener.bind(app)); }
+		return node;
+	}
+	var app = {
+		errors: [],
+		url: "http://vhost3.lnu.se:20080/question/1",
+		init: function(){
+			this.container = document.querySelector(selector);
+			this.container.appendChild((this.msglist = create("div")));
+			var form = create("div");
+			form.appendChild((this.input = create("input",'',this.keyInForm,"keypress")));
+			form.appendChild(create("button",'Skicka!',this.answerQuestion));
+			form.classList.add("form");
+			this.container.appendChild(form);
+			this.container.classList.add("quizgame");
+			this.fetchQuestion();
+		},
+		keyInForm: function(e){
+			if (e.keyCode===13){this.answerQuestion();}
+		},
+		log: function(type,text){
+			var msg = create("div",text);
+			msg.classList.add(type);
+			this.msglist.appendChild(msg);
+		},
+		fetchQuestion: function(){
+			var me = this, xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function(){
+				if (xhr.readyState === 4){
+					me.receiveQuestion(JSON.parse(xhr.responseText));
+				}
+			}
+			xhr.open("GET", this.url, true);
+			xhr.send(null);
+		},
+		receiveQuestion: function(data){
+			this.container.classList.add("hasquestion");
+			this.errors.push(0);
+			this.url = data.nextURL;
+			this.log("question",data.question);
+			this.input.focus();
+		},
+		answerQuestion: function(){
+			var me = this, xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function(){
+				if (xhr.readyState === 4){
+					me.input.value = '';
+					if (xhr.status === 400){
+						me.wrongAnswer();
+					} else {
+						me.correctAnswer(JSON.parse(xhr.responseText));
+					}
+				}
+			}
+			xhr.open("POST", this.url, true);
+			xhr.setRequestHeader('Content-Type', 'application/json')
+			xhr.send(JSON.stringify({answer:this.input.value}));
+		},
+		correctAnswer: function(data){
+			this.log("correct","Yes, det var rätt!");
+			this.container.classList.remove("hasquestion");
+			if (data.nextURL){
+				this.url = data.nextURL;
+				this.fetchQuestion();
+			} else {
+				this.finish();
+			}
+		},
+		wrongAnswer: function(){
+			this.errors[this.errors.length-1] = this.errors[this.errors.length-1]+1;
+			this.log("error","Fel! Försök #"+(this.errors[this.errors.length-1]+1)+":");
+		},
+		finish: function(){
+			this.container.classList.remove("hasquestion");
+			this.log("ending","Du hade totalt "+this.errors.reduce(function(m,i){return m+i;},0)+" fel!");
+		}
+	};
+	app.init();
+};
