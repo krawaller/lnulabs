e374b6faa22de0a5eedf281c1b759b45153ada57
KungD <david@krawaller.se>
2014-12-16 12:37:32 +0100

    initial commit

---
+++ labbymezzage/README.md
@@ -0,0 +1,9 @@
+##Idiomatisk (eller snarare Davidig) implementering av Labby Mezzage
+
+Se labben live [här](http://blog.krawaller.se/jslabs/labbymezzage). Labbinstruktionen finns [här](http://orion.lnu.se/pub/education/course/1DV403/ht12-2/laboration/L02/Laboration%202%20-%20Labby%20Mezzage.pdf).
+
+Jag har bortsett från instruktionen att alla meddelanden ska vara klassinstanser. I JS ligger det ofta närmare till hands att använda statiska funktioner, så som i min lösning.
+
+För att koden inte ska bli alltför bloated när vi bygger element så har jag använt en liten helper-function `create`. I övrigt består appen av en enda objektliteral, där `init`-metoden körs vid initialisering. Den bygger upp noder för formulär och lista, samt sätter event-listeners (som alla är metoder på objektliteralen). Listan av meddelanden, som är vår enda state, levandehålls i en array på literalen.
+
+När UI:t initialt byggs upp så behåller jag också referenser till de noder vi senare behöver komma åt (listan, formuläret). Dessa referenser lagras också på literalen.
---
+++ labbymezzage/labbymezzage.js
@@ -0,0 +1,60 @@
+window.Chat = function(selector){
+	function create(tag,content,listener,event){
+		var node = document.createElement(tag);
+		node.innerHTML = content || "";
+		if (listener){ node.addEventListener(event||"click",listener.bind(app)); }
+		return node;
+	}
+	var app = {
+		msgs: [],
+		init: function(){
+			var container = document.querySelector(selector);
+			container.appendChild((this.msglist = create("div")));
+			container.appendChild((this.counter = create("div")));
+			var form = create("div");
+			form.appendChild((this.input = create("textarea",'',this.keyInForm,"keypress")));
+			form.appendChild(create("button",'Skicka!',this.addMessage));
+			container.appendChild(form);
+			container.classList.add("chatapp");
+			this.drawMessages();
+		},
+		addMessage: function(){
+			this.msgs.push({
+				time: new Date(),
+				text: this.input.value
+			});
+			this.input.value = "";
+			this.drawMessages();
+		},
+		keyInForm: function(e){
+			if (e.keyCode===13 && !e.shiftKey){
+				this.addMessage();
+				e.preventDefault();
+			}
+		},
+		drawMessages: function(){
+			this.msglist.innerHTML = '';
+			this.msgs.map(this.drawMessage.bind(this));
+			this.msglist.appendChild(create("div","Antal: "+this.msgs.length));
+		},
+		drawMessage: function(msg,n){
+			var button = create("button","X",this.deleteMessage.bind(this,n));
+			var time = create("button","?",this.tellTime.bind(this,n));
+			var msg = create("div",this.msgs[n].text.replace(/\n/g,"<br/>"));
+			msg.insertBefore(button,msg.firstChild);
+			msg.insertBefore(time,msg.firstChild);
+			msg.classList.add("message");
+			this.msglist.appendChild(msg);
+		},
+		tellTime: function(n){
+			alert("Created at: "+this.msgs[n].time);
+		},
+		deleteMessage: function(n){
+			if (confirm("Är du helt säker på att du vill radera meddelandet?")){
+				this.msgs.splice(n,1);
+				this.drawMessages();
+			}
+		}
+	};
+	app.init();
+};
